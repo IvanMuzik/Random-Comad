fa10bc0817739e449ca5bd00535ef577f81c943a
IvanMuzik <ivanmuz86@gmail.com>
2022-12-21 23:36:24 +0300

    add vc commands

---
+++ Random Comad/VCEnterComands.swift
@@ -0,0 +1,19 @@
+//
+//  VCEnterComands.swift
+//  Random Comad
+//
+//  Created by Ivan Muzalevski on 21.12.22.
+//
+
+import UIKit
+
+class VCEnterComands: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
